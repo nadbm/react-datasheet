20f5de619aeba29b60aa46fea43652cdb5e651c8
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2018-02-26 11:53:36 -0800

    add sheetRenderer and rowRenderer.

--- types/react-datasheet.d.ts
+++ types/react-datasheet.d.ts
@@ -1,4 +1,4 @@
-import {PureComponent, Component} from "react";
+import {PureComponent, Component, ReactElement, ReactNode} from "react";
 
 declare namespace ReactDataSheet {
     export interface Cell {
@@ -18,7 +18,21 @@ declare namespace ReactDataSheet {
         dataRenderer?: (cell: T) => string | number | void;
         onChange?: (cell: T, row: number, col: number, newValue: string) => any;
         onContextMenu?: (event: MouseEvent, cell: T, i : number, j: number) => any;
-        onCellsChanged?: (arrayOfChanges: {cell: T, row: number, col: number, newValue: string}[]) => void;
+        onCellsChanged?: (arrayOfChanges: {cell: T, row: number, col: number, value: any}[]) => void;
+        sheetRenderer: (props: SheetRendererArgs<T>) => JSX.Element;
+        rowRenderer: (props: RowRendererArgs<T>) => JSX.Element;
+    }
+
+    export interface SheetRendererArgs<T> {
+        data: T[][];
+        className: string;
+        children: ReactNode[];
+    }
+
+    export interface RowRendererArgs<T> {
+        row: number;
+        cells: T[];
+        children: ReactNode[];
     }
 
     export interface CellReference {
@@ -37,7 +51,7 @@ declare namespace ReactDataSheet {
 }
 
 declare class ReactDataSheet<T extends ReactDataSheet.Cell> extends PureComponent<ReactDataSheet.DataSheetProps<T>, ReactDataSheet.DataSheetState> {
-    getSelectedCells(data: T[][], start: ReactDataSheet.CellReference, end: ReactDataSheet.CellReference): {cell: T, row: number, col: number};
+    getSelectedCells: (data: T[][], start: ReactDataSheet.CellReference, end: ReactDataSheet.CellReference) => {cell: T, row: number, col: number};
 }
 
 export default ReactDataSheet;
