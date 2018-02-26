366ba9fb2d3cd30f9392c4bf77e7500cc9f118e9
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2018-02-26 15:50:30 -0800

    Add attributesRenderer and onSelect, and pull out Renderer types.

--- types/react-datasheet.d.ts
+++ types/react-datasheet.d.ts
@@ -14,26 +14,51 @@ declare namespace ReactDataSheet {
     export interface DataSheetProps<T extends Cell> {
         data: T[][];
         className?: string;
-        valueRenderer: (cell: T) => string | number | void;
-        dataRenderer?: (cell: T) => string | number | void;
-        onChange?: (cell: T, row: number, col: number, newValue: string) => any;
+        valueRenderer: ValueRenderer<T>;
+        dataRenderer?: DataRenderer<T>;
+        attributesRenderer?: AttributesRenderer<T>;
+        onChange?: (cell: T, row: number, col: number, newValue: string) => any; //Deprecated!
         onContextMenu?: (event: MouseEvent, cell: T, i : number, j: number) => any;
-        onCellsChanged?: (arrayOfChanges: {cell: T, row: number, col: number, value: any}[]) => void;
-        sheetRenderer: (props: SheetRendererArgs<T>) => JSX.Element;
-        rowRenderer: (props: RowRendererArgs<T>) => JSX.Element;
+        onCellsChanged?: (arrayOfChanges: CellsChangedArg<T>, arrayOfAdditions?: CellAdditionsArg<T>) => void;
+        sheetRenderer?: SheetRenderer<T>;
+        rowRenderer?: RowRenderer<T>;
+        onSelect?: (cell: T) => void;
     }
 
-    export interface SheetRendererArgs<T> {
+    export type ValueRenderer<T extends Cell> = (cell: T, row: number, col: number) => string | number | null | void;
+
+    export type DataRenderer<T extends Cell> = (cell: T, row: number, col: number) => string | number | null | void;
+
+    export type AttributesRenderer<T extends Cell> = (cell: T, row: number, col: number) => {};
+
+    export type SheetRenderer<T extends Cell> = (props: SheetRendererArgs<T>) => JSX.Element;
+    
+    export interface SheetRendererArgs<T extends Cell> {
         data: T[][];
         className: string;
         children: ReactNode[];
     }
 
-    export interface RowRendererArgs<T> {
+    export type RowRenderer<T extends Cell> = (props: RowRendererArgs<T>) => JSX.Element;
+
+    export interface RowRendererArgs<T extends Cell> {
         row: number;
         cells: T[];
         children: ReactNode[];
     }
+
+    export type CellsChangedArg<T extends Cell> = {
+        cell: T;
+        row: number;
+        col: number;
+        value: string | number | null; //is this ever undefined?
+    }[]
+
+    export type CellAdditionsArg<T extends Cell> = {
+        row: number;
+        col: number;
+        value: string | number | null; //is this ever undefined?
+    }[]
 
     export interface CellReference {
         row: number;
@@ -51,7 +76,7 @@ declare namespace ReactDataSheet {
 }
 
 declare class ReactDataSheet<T extends ReactDataSheet.Cell> extends PureComponent<ReactDataSheet.DataSheetProps<T>, ReactDataSheet.DataSheetState> {
-    getSelectedCells: (data: T[][], start: ReactDataSheet.CellReference, end: ReactDataSheet.CellReference) => {cell: T, row: number, col: number};
+        getSelectedCells: (data: T[][], start: ReactDataSheet.CellReference, end: ReactDataSheet.CellReference) => {cell: T, row: number, col: number};
 }
 
 export default ReactDataSheet;
