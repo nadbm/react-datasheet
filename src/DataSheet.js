6d9a11589761bc7528c61a5acd321f6e42a290dc
fakiyer <fakiyer@gmail.com>
2018-06-05 15:44:40 +0900

    セルに値を入力した後、別のセルをクリックした際に、入力できなくなるIEの問題への対処

--- src/DataSheet.js
+++ src/DataSheet.js
@@ -258,6 +258,9 @@ export default class DataSheet extends PureComponent {
       110
     ].indexOf(keyCode) > -1
 
+    // セルに値を入力した後、別のセルをクリックした際に、入力できなくなるIEの問題への対処
+    document.removeEventListener('keydown', this.handleKey)
+
     if (noCellsSelected || ctrlKeyPressed) {
       return true
     }
@@ -382,6 +385,9 @@ export default class DataSheet extends PureComponent {
     let editing = (isEmpty(this.state.editing) || this.state.editing.i !== i || this.state.editing.j !== j)
       ? {} : this.state.editing
     this._setState({selecting: true, start: {i, j}, end: {i, j}, editing: editing, forceEdit: false})
+
+    // セルに値を入力した後、別のセルをクリックした際に、入力できなくなるIEの問題への対処
+    document.addEventListener('keydown', this.handleKey)
 
     // Keep listening to mouse if user releases the mouse (dragging outside)
     document.addEventListener('mouseup', this.onMouseUp)
