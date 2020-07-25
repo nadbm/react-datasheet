632c314b00a7bf5653130d0ba6c254dd176067b6
Nadim Islam <nadim.islam@aptible.com>
2020-07-25 13:54:28 -0400

    update example

--- docs/src/examples/BasicSheet.js
+++ docs/src/examples/BasicSheet.js
@@ -3,38 +3,67 @@ import React from 'react';
 import Datasheet from '../lib/DataSheet';
 
 export default class BasicSheet extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       grid: [
         [
-          {readOnly: true, value: ''},
-          {value: 'A', readOnly: true},
-          {value: 'B', readOnly: true},
-          {value: 'C', readOnly: true},
-          {value: 'D', readOnly: true}
+          { readOnly: true, value: '' },
+          { value: 'A', readOnly: true },
+          { value: 'B', readOnly: true },
+          { value: 'C', readOnly: true },
+          { value: 'D', readOnly: true },
         ],
-        [{readOnly: true, value: 1}, {value: 1}, {value: 3}, {value: 3}, {value: 3}],
-        [{readOnly: true, value: 2}, {value: 2}, {value: 4}, {value: 4}, {value: 4}],
-        [{readOnly: true, value: 3}, {value: 1}, {value: 3}, {value: 3}, {value: 3}],
-        [{readOnly: true, value: 4}, {value: 2}, {value: 4}, {value: 4}, {value: 4}]
-      ]
-    }
+        [
+          { readOnly: true, value: 1 },
+          { value: 1 },
+          { value: 3 },
+          { value: 3 },
+          { value: 3 },
+        ],
+        [
+          { readOnly: true, value: 2 },
+          { value: 2 },
+          { value: 4 },
+          { value: 4 },
+          { value: 4 },
+        ],
+        [
+          { readOnly: true, value: 3 },
+          { value: 1 },
+          { value: 3 },
+          { value: 3 },
+          { value: 3 },
+        ],
+        [
+          { readOnly: true, value: 4 },
+          { value: 2 },
+          { value: 4 },
+          { value: 4 },
+          { value: 4 },
+        ],
+      ],
+    };
   }
-  render () {
+  valueRenderer = cell => cell.value;
+  onCellsChanged = changes => {
+    const grid = this.state.grid;
+    changes.forEach(({ cell, row, col, value }) => {
+      grid[row][col] = { ...grid[row][col], value };
+    });
+    this.setState({ grid });
+  };
+  onContextMenu = (e, cell, i, j) =>
+    cell.readOnly ? e.preventDefault() : null;
+
+  render() {
     return (
       <Datasheet
         data={this.state.grid}
-        valueRenderer={(cell) => cell.value}
-        onContextMenu={(e, cell, i, j) => cell.readOnly ? e.preventDefault() : null}
-        onCellsChanged={changes => {
-          const grid = this.state.grid.map(row => [...row])
-          changes.forEach(({cell, row, col, value}) => {
-            grid[row][col] = {...grid[row][col], value}
-          })
-          this.setState({grid})
-        }}
+        valueRenderer={this.valueRenderer}
+        onContextMenu={this.onContextMenu}
+        onCellsChanged={this.onCellsChanged}
       />
-    )
+    );
   }
 }
